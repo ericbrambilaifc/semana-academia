866c5c63598a0dd98d95d4ada00b4315cc1805e1
ericbrambilaifc <143406395+ericbrambilaifc@users.noreply.github.com>
2023-10-31 13:02:13 +0000

    meeee

---
+++ script-orcamento.js
@@ -0,0 +1,14 @@
+let chave = 'inscrição';
+localStorage.setItem(chave, 'valor');
+
+let minhaInscrição = localStorage.getItem(chave);
+
+localStorage.setItem(chave, 'novo valor');
+
+localStorage.removeItem(chave);
+
+localStorage.clear();
+
+function soma(){    
+    let cafe = quantidade
+}
