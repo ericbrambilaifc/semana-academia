a77eb5f0540529771f638b3300fd8e9cbc1812bb
ericbrambilaifc <143406395+ericbrambilaifc@users.noreply.github.com>
2023-10-31 18:04:51 +0000

    ta foda

--- script-orcamento.js
+++ script-orcamento.js
@@ -1,14 +1,28 @@
-let chave = 'inscrição';
-localStorage.setItem(chave, 'valor');
+function salvarDados() {
+    let numeroParticipantes = parseInt(localStorage.getItem('numeroParticipantes')) || 0;
+  
+    const numeroParticipantesInput = document.getElementById('numeroParticipantes');
+    const numeroParticipantesValue = parseInt(numeroParticipantesInput.value);
+  
+    if (!isNaN(numeroParticipantesValue)) {
+      numeroParticipantes += numeroParticipantesValue;
+      localStorage.setItem('numeroParticipantes', numeroParticipantes);
+  
+      const numeroParticipantesDisplay = document.getElementById('numeroParticipantesDisplay');
+      numeroParticipantesDisplay.textContent = 'Número total de participantes: ' + numeroParticipantes;
+  
+      numeroParticipantesInput.value = '';   //limpa o campo de entrada
+    } else {
+      alert('Por favor, insira um número válido.');
+    }
+  }
 
-let minhaInscrição = localStorage.getItem(chave);
-
-localStorage.setItem(chave, 'novo valor');
-
-localStorage.removeItem(chave);
-
-localStorage.clear();
-
-function soma(){    
-    let cafe = quantidade
-}
+  function adicionarParticipante() {
+    const numeroParticipantesInput = document.getElementById('numeroParticipantes');
+    const numeroParticipantesValue = parseInt(numeroParticipantesInput.value);
+  
+    const numeroParticipantesDisplay = document.getElementById('numeroParticipantesDisplay');
+    numeroParticipantesDisplay.textContent =  + numeroParticipantesValue;
+  
+    numeroParticipantesInput.value = '';
+  }
