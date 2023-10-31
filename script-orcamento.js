943cc4263062844a25043b1f18efa804832f28ee
ericbrambilaifc <143406395+ericbrambilaifc@users.noreply.github.com>
2023-10-31 19:23:32 +0000

    não sei oq to fazendo

--- script-orcamento.js
+++ script-orcamento.js
@@ -1,41 +1,129 @@
+//LocalStorage
 function salvarDados() {
-    const numeroParticipantesInput = document.getElementById('numeroParticipantes');
-    const numeroParticipantesValue = parseInt(numeroParticipantesInput.value);
-  
-    if (!isNaN(numeroParticipantesValue)) {
-      const numeroParticipantes = (parseInt(localStorage.getItem('numeroParticipantes')) || 0) + numeroParticipantesValue;
-      localStorage.setItem('numeroParticipantes', numeroParticipantes);
-  
-      document.getElementById('numeroParticipantesDisplay').textContent = 'Número total de participantes: ' + numeroParticipantes;
-  
-      numeroParticipantesInput.value = '';
-    } else {
-      alert('Por favor, insira um número válido.');
-    }
+  const totalInscritosInput = document.getElementById('numeroParticipantes');
+  const totalInscritosValue = parseInt(totalInscritosInput.value);
+
+  if (!isNaN(totalInscritosValue)) {
+    const totalInscritos = (parseInt(localStorage.getItem('totalInscritos')) || 0) + totalInscritosValue;
+    localStorage.setItem('totalInscritos', totalInscritos);
+
+    document.getElementById('numeroParticipantesVizualizacao').textContent = 'Número total de inscritos: ' + totalInscritos;
+
+    totalInscritosInput.value = '';
+  } else {
+    alert('Por favor, insira um número válido.');
   }
-  
-  function adicionarParticipante() {
-    const numeroParticipantesInput = document.getElementById('numeroParticipantes');
-    const numeroParticipantesValue = parseInt(numeroParticipantesInput.value);
-  
-    if (!isNaN(numeroParticipantesValue)) {
-      const numeroParticipantes = parseInt(localStorage.getItem('numeroParticipantes')) || 0;
-      const novoNumeroParticipantes = numeroParticipantes + numeroParticipantesValue;
-  
-      localStorage.setItem('numeroParticipantes', novoNumeroParticipantes);
-  
-      document.getElementById('numeroParticipantesDisplay').textContent = novoNumeroParticipantes;
-  
-      numeroParticipantesInput.value = '';
-    } else {
-      alert('Por favor, insira um número válido.');
-    }
+}
+
+function adicionarParticipante() {
+  const totalInscritosInput = document.getElementById('numeroParticipantes');
+  const totalInscritosValue = parseInt(totalInscritosInput.value);
+
+  if (!isNaN(totalInscritosValue)) {
+    const totalInscritos = parseInt(localStorage.getItem('totalInscritos')) || 0;
+    const novoTotalInscritos = totalInscritos + totalInscritosValue;
+
+    localStorage.setItem('totalInscritos', novoTotalInscritos);
+
+    document.getElementById('numeroParticipantesVizualizacao').textContent = novoTotalInscritos;
+
+    totalInscritosInput.value = '';
+  } else {
+    alert('Por favor, insira um número válido.');
   }
-  
-  window.onload = function() {
-    const numeroParticipantes = localStorage.getItem('numeroParticipantes');
-    if (numeroParticipantes !== null) {
-      document.getElementById('numeroParticipantesDisplay').textContent = numeroParticipantes;
-    }
+}
+
+window.onload = function() {
+  const totalInscritos = localStorage.getItem('totalInscritos');
+  if (totalInscritos !== null) {
+    document.getElementById('numeroParticipantesVizualizacao').textContent = totalInscritos;
   }
-   
+}
+//-------------------------------------- LocalStorage
+
+// contas 
+function calcularTotal(quantidade, preco, total) {
+  // Pega os elementos de input com base nos IDs fornecidos
+  const quantidadeInput = document.getElementById(quantidade);
+  const precoInput = document.getElementById(preco);
+  const totalInput = document.getElementById(total);
+
+  // Adiciona um ouvinte de evento para atualizar o total quando a quantidade muda
+  quantidadeInput.addEventListener('input', () => {
+    // Pega a quantidade e o preço, ou assume 0 se não forem números válidos
+    const quantidade = parseFloat(quantidadeInput.value) || 0;
+    const preco = parseFloat(precoInput.value) || 0;
+
+    // Calcula o novo total
+    const total = quantidade * preco;
+
+    // Atualiza o valor no campo de total
+    totalInput.value = total;
+  });
+
+  // Adiciona um ouvinte de evento para atualizar o total quando o preço muda
+  precoInput.addEventListener('input', () => {
+    // Pega a quantidade e o preço, ou assume 0 se não forem números válidos
+    const quantidade = parseFloat(quantidadeInput.value) || 0;
+    const preco = parseFloat(precoInput.value) || 0;
+
+    // Calcula o novo total
+    const total = quantidade * preco;
+
+    // Atualiza o valor no campo de total
+    totalInput.value = total;
+  });
+
+  // O seguinte trecho não é necessário (comentado)
+  // const quantidade = parseFloat(quantidadeInput.value) || 0;
+  // const preco = parseFloat(precoInput.value) || 0;
+  // const total = quantidade * preco;
+  // totalInput.value = total;
+}
+
+// Chama a função para calcular o total para cada item da tabela
+calcularTotal('quantidadeCafe', 'precoCafe', 'totalCafe');
+calcularTotal('quantidadeBolo', 'precoBolo', 'totalBolo');
+calcularTotal('quantidadeCachorro', 'precoCachorro', 'totalCachorro');
+calcularTotal('quantidadeBanner', 'precoBanner', 'totalBanner');
+calcularTotal('quantidadeCracha', 'precoCracha', 'totalCracha');
+calcularTotal('quantidadeCamiseta', 'precoCamiseta', 'totalCamiseta');
+//--------------------------------------
+
+//salvar os dados
+
+// Crie um array vazio para armazenar os dados
+const dadosGuardados = [];
+
+// Função para salvar os dados quando o botão é clicado
+function dadosGuardados() {
+  // Obtenha os valores dos campos
+  const quantidadeCafe = parseFloat(document.getElementById('quantidadeCafe').value) || 0;
+  const precoCafe = parseFloat(document.getElementById('precoCafe').value) || 0;
+  const totalCafe = parseFloat(document.getElementById('totalCafe').value) || 0;
+
+  // Crie um objeto com os dados
+  const dados = {
+    nomeProduto: 'Café',
+    quantidade: quantidadeCafe,
+    preco: precoCafe,
+    total: totalCafe,
+  };
+
+  // Adicione o objeto ao array
+  dadosGuardados.push(dados);
+
+  // Limpe os campos após salvar os dados
+  document.getElementById('quantidadeCafe').value = '';
+  document.getElementById('precoCafe').value = '';
+  document.getElementById('totalCafe').value = '';
+
+  // Você pode exibir os dados salvos em uma área na página se desejar
+  exibirDadosGuardados();
+}
+
+// Função para exibir os dados salvos em uma área na página
+function exibirDadosGuardados() {
+  const dadosGuardadosDisplay = document.getElementById('dadosGuardadosDisplay');
+  dadosGuardadosDisplay.textContent = JSON.stringify(dadosGuardados, null, 2);
+}
